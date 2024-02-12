502e4ded15829fbeb36dc5ed1bbaf305d7488a1b
Evelyn <evelynlawyeeting@gmail.com>
2024-02-12 16:54:19 -0500

    Updates

---
+++ precache-manifest.d8a39dfce577436825e50f4a0d7497a6.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b330eac2be89ceec371413f8e825835e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebb92749cb39d8ae0558",
+    "url": "/static/css/main.3d200c6f.chunk.css"
+  },
+  {
+    "revision": "f298c7e1485080c4ddb6",
+    "url": "/static/js/2.f07dec75.chunk.js"
+  },
+  {
+    "revision": "b14cf38e36b679bf3c7c",
+    "url": "/static/js/3.bb0829b5.chunk.js"
+  },
+  {
+    "revision": "ebb92749cb39d8ae0558",
+    "url": "/static/js/main.3b8c70c9.chunk.js"
+  },
+  {
+    "revision": "d4213f11947feb9ca805",
+    "url": "/static/js/runtime~main.638c8875.js"
+  },
+  {
+    "revision": "0f0ea204488f7d0bbaa7451c312f4db5",
+    "url": "/static/media/Comfortaa-VariableFont_wght.0f0ea204.ttf"
+  },
+  {
+    "revision": "ef2de151531136733158a6005a5ac82b",
+    "url": "/static/media/Hubballi-Regular.ef2de151.ttf"
+  },
+  {
+    "revision": "2f2d8e820af11e6c7d332ea1723a18af",
+    "url": "/static/media/Nelphim.2f2d8e82.otf"
+  },
+  {
+    "revision": "7cf6ea367a77180f81347ab5abae3c04",
+    "url": "/static/media/brand name.7cf6ea36.svg"
+  },
+  {
+    "revision": "e1ef7d6eecbe4c736d052b48c679a417",
+    "url": "/static/media/business cards.e1ef7d6e.png"
+  },
+  {
+    "revision": "1578da1a342215fb7f9fbc6670a0a97f",
+    "url": "/static/media/ccs tea set.1578da1a.svg"
+  },
+  {
+    "revision": "0183660fb8248e3ecc165f914425c02f",
+    "url": "/static/media/fonts.0183660f.svg"
+  },
+  {
+    "revision": "1b30241dd645c0771aded5dd296dc93c",
+    "url": "/static/media/genshin.1b30241d.png"
+  },
+  {
+    "revision": "e7b659e3f5cb7da48ec4c3090c38e96d",
+    "url": "/static/media/image.e7b659e3.png"
+  },
+  {
+    "revision": "d0250f0f354181ac81842ddd93d48566",
+    "url": "/static/media/logo draft 2.d0250f0f.png"
+  },
+  {
+    "revision": "70108579be4da89b2f27046bc7285a35",
+    "url": "/static/media/logo draft.70108579.png"
+  },
+  {
+    "revision": "5331e23fb87dffd10c86742475e343f3",
+    "url": "/static/media/logo final.5331e23f.png"
+  },
+  {
+    "revision": "386caddfa29145372b5887371e7d9665",
+    "url": "/static/media/logo font.386caddf.png"
+  },
+  {
+    "revision": "379cea829afc9d6f45fcd3253b4c545f",
+    "url": "/static/media/logo.379cea82.svg"
+  },
+  {
+    "revision": "9f0b6f7b0670aa7f2786132d566600fa",
+    "url": "/static/media/mascot cats.9f0b6f7b.png"
+  },
+  {
+    "revision": "880c27f72f027dd01f708b572fbfe9e5",
+    "url": "/static/media/mascot draft.880c27f7.png"
+  },
+  {
+    "revision": "0a713459697f78d62bdf3eac0374fd41",
+    "url": "/static/media/mascot final.0a713459.png"
+  }
+]);
